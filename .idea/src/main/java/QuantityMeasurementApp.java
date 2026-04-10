e8e5171b7b2054d95fee79734e3dad24366df4bf
i-rajveer-singh <work.rajveerbagga@gmail.com>
2026-04-10 13:50:48 +0530

    [Rajveer]:UC3:creating quantity length class to compare any unit

--- .idea/src/main/java/QuantityMeasurementApp.java
+++ .idea/src/main/java/QuantityMeasurementApp.java
@@ -1,8 +1,8 @@
 public class QuantityMeasurementApp {
     public static void main(String[] args) {
-        Feet f = new Feet(1.0);
-        Inches i = new Inches(12.0);
+        QuantityLength q1 = new QuantityLength(1.0, "FEET");
+        QuantityLength q2 = new QuantityLength(12.0, "INCH");
 
-        System.out.println(f.equals(i)); // true
+        System.out.println(q1.equals(q2)); // true
     }
 }
